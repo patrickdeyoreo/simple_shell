b5dd6cbce2424523fef30ded838a622f2721561c
patrickdeyoreo <pdeyoreo@gmail.com>
2019-08-27 13:38:59 -0700

    Fixed cd environment updating and error handling

--- _cd.c
+++ _cd.c
@@ -1,59 +1,49 @@
 #include "shell.h"
 /**
-  * _cd - changes the directory
-  * @info: arguments passed
-  * @Return: int
-  */
+ * _cd - changes the directory
+ * @info: arguments passed
+ * @Return: int
+ */
 int _cd(info_t *info)
 {
-	char *dir;
-	char *setenv_tokens[] = { "setenv", "OLDPWD", info->cwd, NULL };
 	char **tokens = info->tokens;
+	char *setenv_tokens[] = { "setenv", "OLDPWD", info->cwd, NULL };
+	char *dir;
+
+	info->tokens = setenv_tokens;
 
 	if (tokens[1])
 	{
-		info->tokens = setenv_tokens;
-		if (_strcmp(tokens[1], "-") == 0)
-		{
-			dir = _getenv(info->env, "OLDPWD");
-			if (!dir)
-			{
-				chdir(".");
-				_setenv(info);
-			}
-			else
-			{
-				chdir(dir);
-				_setenv(info);
-				free(info->cwd);
-				info->cwd = getcwd(NULL, 0);
-			}
-		}
+		dir = _getenv(info->env, "OLDPWD");
+		if (_strcmp(tokens[1], "-") != 0)
+			info->status = chdir(tokens[1]);
+		else if (dir)
+			info->status = chdir(dir);
 		else
-		{
-			chdir(tokens[1]);
-			_setenv(info);
-			free(info->cwd);
-			info->cwd = getcwd(NULL, 0);
-		}
+			info->status = chdir(".");
 	}
 	else
 	{
 		dir = _getenv(info->env, "HOME");
 		if (dir)
-		{
-			chdir(dir);
-			_setenv(info);
-			free(info->cwd);
-			info->cwd = getcwd(NULL, 0);
-		}
+			info->status = chdir(dir);
 	}
-	setenv_tokens[1] = "PWD";
-	setenv_tokens[2] = info->cwd;
-	_setenv(info);
+	if (info->status < 0)
+	{
+		perror(info->argv[0]);
+		info->status = EXIT_FAILURE;
+	}
+	else
+	{
+		_setenv(info);
+		free(info->cwd);
+		info->cwd = getcwd(NULL, 0);
+		setenv_tokens[1] = "PWD";
+		setenv_tokens[2] = info->cwd;
+		_setenv(info);
+		info->status = EXIT_SUCCESS;
+	}
+
 	info->tokens = tokens;
-	info->status = EXIT_SUCCESS;
-
 	return (info->status);
-
 }
