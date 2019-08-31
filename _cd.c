7f10250524d57fe39da54e07808104219ef1ec67
patrickdeyoreo <pdeyoreo@gmail.com>
2019-08-30 22:58:28 -0700

    setenv builtin now performs no action when given less than 2 args

--- _cd.c
+++ _cd.c
@@ -1,9 +1,37 @@
 #include "shell.h"
 
+
+/**
+ * _cd_success - update the environment upon success
+ * @info: shell info
+ */
+void _cd_success(info_t *info)
+{
+	char **tokens = info->tokens;
+	char *setenv_tokens[] = { "setenv", NULL, NULL, NULL };
+
+	info->tokens = setenv_tokens;
+
+	setenv_tokens[1] = "OLDPWD";
+	setenv_tokens[2] = info->cwd;
+	_setenv(info);
+
+	free(info->cwd);
+	info->cwd = getcwd(NULL, 0);
+
+	setenv_tokens[1] = "PWD";
+	setenv_tokens[2] = info->cwd;
+	_setenv(info);
+
+	info->tokens = tokens;
+	info->status = EXIT_SUCCESS;
+}
+
+
 /**
  * _cd_error - print an error upon failure to change directory
  * @info: shell info
- * @dir: directory name
+ * @dir: directory
  */
 void _cd_error(info_t *info, char *dir)
 {
@@ -11,27 +39,15 @@ void _cd_error(info_t *info, char *dir)
 
 	cmd_num = num_to_str(info->cmd_num);
 	error = strjoin("can't cd to", dir, ' ');
-	_perror(4, info->argv[0], cmd_num, "cd", error);
+
+	_perror(4, info->argv[0], cmd_num, info->tokens[0], error);
+
 	free(cmd_num);
 	free(error);
+
 	info->status = 2;
 }
 
-/**
- * _cd_success - update the environment upon success
- * @info: shell info
- * @setenv_tokens: tokens to set the environment
- */
-void _cd_success(info_t *info, char **setenv_tokens)
-{
-	_setenv(info);
-	free(info->cwd);
-	info->cwd = getcwd(NULL, 0);
-	setenv_tokens[1] = "PWD";
-	setenv_tokens[2] = info->cwd;
-	_setenv(info);
-	info->status = EXIT_SUCCESS;
-}
 
 /**
  * _cd - changes the directory
@@ -41,28 +57,27 @@ void _cd_success(info_t *info, char **setenv_tokens)
  */
 int _cd(info_t *info)
 {
-	char **tokens = info->tokens;
-	char *setenv_tokens[] = { "setenv", "OLDPWD", NULL, NULL };
 	char *dir;
 
-	info->tokens = setenv_tokens;
-	setenv_tokens[2] = info->cwd;
-
-	if (tokens[1])
+	if (info->tokens[1])
 	{
-		dir = _getenv(info->env, "OLDPWD");
-		if (_strcmp(tokens[1], "-") != 0)
+		if (_strcmp(info->tokens[1], "-"))
 		{
-			dir = tokens[1];
+			dir = info->tokens[1];
 			info->status = chdir(dir);
 		}
 		else
 		{
+			dir = _getenv(info->env, "OLDPWD");
 			if (!dir)
 				dir = info->cwd;
+
 			info->status = chdir(dir);
-			write(STDOUT_FILENO, dir, _strlen(dir));
-			write(STDOUT_FILENO, "\n", 1);
+			if (!info->status)
+			{
+				write(STDOUT_FILENO, dir, _strlen(dir));
+				write(STDOUT_FILENO, "\n", 1);
+			}
 		}
 	}
 	else
@@ -70,12 +85,14 @@ int _cd(info_t *info)
 		dir = _getenv(info->env, "HOME");
 		if (dir)
 			info->status = chdir(dir);
+		else
+			info->status = 0;
 	}
-	if (info->status < 0)
+
+	if (info->status == -1)
 		_cd_error(info, dir);
 	else
-		_cd_success(info, setenv_tokens);
+		_cd_success(info);
 
-	info->tokens = tokens;
 	return (info->status);
 }
